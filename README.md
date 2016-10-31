c1502287b5be9a3942a4bd6a9fff25b4f76f35a3
NLKNguyen <NLKNguyen@MSN.com>
2016-10-30 21:03:50 -0700

    Move cluster setup to this repository

--- README.md
+++ README.md
@@ -1,116 +1,29 @@
-# Alpine-MPICH
-* `3.2`, `latest` ([Dockerfile](https://github.com/NLKNguyen/alpine-mpich/blob/master/Dockerfile))
-* `3.2-onbuild`, `onbuild` ([Dockerfile](https://github.com/NLKNguyen/alpine-mpich/blob/onbuild/Dockerfile))
+# Alpine MPICH
 
-Docker image of Alpine Linux with latest version of [MPICH](http://www.mpich.org/) -- portable implementation of Message Passing Interface (**MPI**) standard.
+Docker image of Alpine Linux with  [MPICH](http://www.mpich.org/) -- portable implementation of Message Passing Interface (**MPI**) standard.
+Designed for MPI program development and deployment.
 
-#### Include:  
-  * MPICH compiler (the version is according to the tag version)
-  * build-base package (gcc, g++, make, wget, curl, etc.) and openssh
-  * default user `mpi` (sudoer without password)
-  * default working directory `/project` (owned by default user)
+For usage instruction, see the Docker Hub page of this image: 
+[https://hub.docker.com/r/nlknguyen/alpine-mpich](https://hub.docker.com/r/nlknguyen/alpine-mpich/).
 
-The image is available [here](https://hub.docker.com/r/nlknguyen/alpine-mpich/) at DockerHub and is automatically rebuilt by changes on [this](https://github.com/NLKNguyen/alpine-mpich) GitHub repository. This image is intended for developing projects that use MPICH and unlikely to be suitable for developing MPICH itself.
+----
+Automated build with Travis CI and push to Docker Hub
 
-![screencast](https://cloud.githubusercontent.com/assets/4667129/17387984/7b4f024e-59ad-11e6-8146-f730345d6d6f.gif)
-
-# Usage
-
-## For Development
-
-At the directory of your MPI project, run:
-
-```sh
-$ docker run --rm -it -v $(pwd):/project nlknguyen/alpine-mpich
-```
-
-- After downloading the image to the cache in your system for the first time, it will fire up a Docker container based on this image, and, without additional argument, it drops you in an interactive shell (ASH) where you can run `mpicc`, `mpirun`, or any software available in the container.
-- Your project directory is mounted to `/project` directory inside the container so that the programs in the container can have effects on your project directory. This way you can develop your MPI program using any editor in any host OS and use this Docker container to compile and run the MPI program.
-
-Argument explanation:
-* `--rm` remove the container after the program is finished.
-* `-it` open an interactive terminal session (see [-i -t](https://docs.docker.com/engine/reference/run/) for details)
-* `-v $(pwd):/project` volume mount the current directory `$(pwd)` where your shell is at, to the directory `/project` inside the container.
-
-*Follow general guidelines for using Docker container*
-
-To get updated image:
-
-```sh
-$ docker pull nlknguyen/alpine-mpich
-```
-
-## Extending the Image
-It is common that you need to install/remove packages, add compiled program, configure network, or any administration task. To do that, create your own Dockerfile and extend from this image. Below is a simple example. See Docker documentation for details.
+[![Build Status](https://travis-ci.org/NLKNguyen/alpine-mpich.svg?branch=master)](https://travis-ci.org/NLKNguyen/alpine-mpich)
 
 
-Example: add packages
 
-Create your own `Dockerfile` with the content:
+`base image` ([Dockerfile](https://github.com/NLKNguyen/alpine-mpich/blob/master/Dockerfile)) : contains MPICH and essential build tools. Intended to be used as development environment for developing MPI programs.
 
-```Dockerfile
-FROM nlknguyen/alpine-mpich:3.2
+`onbuild image` ([Dockerfile](https://github.com/NLKNguyen/alpine-mpich/blob/onbuild/Dockerfile)) : inherits base image with network setup for cluster. Can be used like base image but intended to be used to build image that contains compiled MPI program in order to deploy to a cluster.
 
-RUN sudo apk add --no-cache valgrind gdb
 
-# if you need to run as root
-USER root
+----
 
-# run commands as root
 
-# switch back to non-root user
-USER ${USER}
+## Feedback
 
-CMD ["/bin/ash"]
-```
-
-then build
-
-```sh
-$ docker build -t my-custom-image .
-```
-
-*Build arguments are available. See the next section.*
-
-to run:
-
-```sh
-$ docker run --rm -it -v $(pwd):/project my-custom-image
-```
-
-Some **environment variables** from the image that you can use in your Dockerfile:
-- `USER` *non-root user who is a sudoer without password. Default=`mpi`*
-- `WORKDIR` *main working directory owned by default user. Default=`/project`*
-
-*However, these are not intended to be set at Docker run command. They can be set at build time, and their meaningful values stay permanent.*
-
-## Build Customization
-
-In order to customize the image at build time, you need to download the Dockerfile source code and build with optional build arguments (without those, you get the exact image as you pull from DockerHub), for example:
-
-```sh
-$ git clone https://github.com/NLKNguyen/alpine-mpich
-
-$ cd alpine-mpich
-
-$ docker build --build-arg MPICH_VERSION="3.2b4" -t my-custom-image .
-```
-
-These are available **build arguments** to customize the build:
-- `REQUIRE` *space-separated names of packages to be installed from Alpine main [package repository](http://pkgs.alpinelinux.org/packages) before downloading and installing MPICH. Default=`"sudo build-base openssh"`*
-- `MPICH_VERSION` *to find which version of MPICH to download from [here](http://www.mpich.org/static/downloads/). Default=`"3.2"`*
-- `MPICH_CONFIGURE_OPTIONS` *to be passed to `./configure` in MPICH source directory. Default=`"--disable-fortran"`* (let me know if you need Fortran, I can certainly add it by default)
-- `MPICH_MAKE_OPTIONS` *to be passed to `make` after the above command. Default is empty*
-- `USER` *non-root user with sudo privilege and no password required. Default=`mpi`*
-- `WORKDIR` *main working directory to be owned by default user. Default=`/project`*
-
-*See MPICH documentation for available options*
-
-Should you need more than that, you need to change the Dockerfile yourself or send suggestion/pull requests to this GitHub repository.
-
-# Feedback
-
-Feedbacks are always welcome. For general comments, use the comment section at the bottom of this image page on DockerHub
+Feedbacks are always welcome. For general comments, use the comment section at the bottom of this [image page](https://hub.docker.com/r/nlknguyen/alpine-mpich) on Docker Hub
 
 ## Issue
 
---
+++ cluster/cluster.sh
@@ -0,0 +1,360 @@
+#!/bin/sh
+
+# The MIT License (MIT)
+# 
+# Copyright (c) 2016 Nikyle Nguyen
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Origin: https://github.com/NLKNguyen/alpine-mpich-cluster
+
+set -e
+
+# shellcheck disable=SC1091
+. ./.env
+
+#######################
+# TASK INDICATORS
+COMMAND_UP=0
+COMMAND_DOWN=0
+COMMAND_RELOAD=0
+COMMAND_SCALE=0
+COMMAND_LOGIN=0
+COMMAND_EXEC=0
+COMMAND_LIST=0
+
+
+# Default values if providing empty
+SIZE=4
+
+#############################################
+usage ()
+{
+    echo " Alpine MPICH Cluster"
+    echo ""
+    echo " USAGE: ./cluster.sh [COMMAND] [OPTIONS]"
+    echo ""
+    echo " Examples of [COMMAND] can be:"
+    echo "      up: start cluster"
+    echo "          ./cluster.sh up size=10"
+    echo ""    
+    echo "      scale: resize the cluster"
+    echo "          ./cluster.sh scale size=30"
+    echo ""
+    echo "      reload: rebuild image and distribute to nodes"
+    echo "          ./cluster.sh reload size=15"
+    echo ""
+    echo "      login: login to Docker container of MPI master node for interactive usage"
+    echo "          ./cluster.sh login"
+    echo ""
+    echo "      exec: execute shell command at the MPI master node"
+    echo "          ./cluster.sh exec [SHELL COMMAND]"    
+    echo ""     
+    echo "      down: shutdown cluster"
+    echo "          ./cluster.sh down"
+    echo ""
+    echo "      list: show running containers of cluster"
+    echo "          ./cluster.sh list"
+    echo ""    
+    echo "      help: show this message"
+    echo "          ./cluster.sh help"
+    echo ""    
+    echo "  "
+}
+
+HEADER="
+         __v_
+        (.___\/{
+~^~^~^~^~^~^~^~^~^~^~^~^~\n"
+
+down_all ()
+{
+    printf "\n\n===> CLEAN UP CLUSTER"
+
+    printf "\n%s\n" "$HEADER"
+    echo "$ docker-compose down"
+    printf "\n"
+
+    docker-compose down
+}
+
+up_registry ()
+{
+    printf "\n\n===> SPIN UP REGISTRY"
+
+    printf "\n%s\n" "$HEADER"
+    echo "$ docker-compose up -d registry"
+    printf "\n"
+
+    docker-compose up -d registry
+}
+
+generate_ssh_keys ()
+{
+    if [ -f ssh/id_rsa ] && [ -f ssh/id_rsa.pub ]; then
+        return 0
+    fi
+    
+    printf "\n\n===> GENERATE SSH KEYS \n\n"
+
+    echo "$ mkdir -p ssh/ "
+    printf "\n"
+    mkdir -p ssh/
+
+    echo "$ ssh-keygen -f ssh/id_rsa -t rsa -N ''"
+    printf "\n"
+    ssh-keygen -f ssh/id_rsa -t rsa -N ''
+}
+
+build_and_push_image ()
+{
+    printf "\n\n===> BUILD IMAGE"
+    printf "\n%s\n" "$HEADER"
+    echo "$ docker build -t \"$REGISTRY_ADDR:$REGISTRY_PORT/$IMAGE_NAME\" ."
+    printf "\n"
+    docker build -t "$REGISTRY_ADDR:$REGISTRY_PORT/$IMAGE_NAME" .
+
+    printf "\n"
+
+    printf "\n\n===> PUSH IMAGE TO REGISTRY"
+    printf "\n%s\n" "$HEADER"
+    echo "$ docker push \"$REGISTRY_ADDR:$REGISTRY_PORT/$IMAGE_NAME\""
+    printf "\n"
+    docker push "$REGISTRY_ADDR:$REGISTRY_PORT/$IMAGE_NAME"
+}
+
+up_master ()
+{
+    printf "\n\n===> SPIN UP MASTER NODE"
+    printf "\n%s\n" "$HEADER"
+    echo "$ docker-compose up -d master"
+    printf "\n"
+    docker-compose up -d master
+}
+
+
+up_workers ()
+{
+    printf "\n\n===> SPIN UP WORKER NODES"
+    printf "\n%s\n" "$HEADER"
+    echo "$ docker-compose up -d worker"
+    printf "\n"
+    docker-compose up -d worker
+
+    printf "\n"
+    printf "\n%s\n" "$HEADER"
+
+    NUM_WORKER=$((SIZE - 1))
+    echo "$ docker-compose scale worker=$NUM_WORKER"
+    printf "\n"
+    docker-compose scale worker=${NUM_WORKER}
+}
+
+down_master ()
+{
+    printf "\n\n===> TORN DOWN MASTER NODE"
+    printf "\n%s\n" "$HEADER"
+
+    echo "$ docker-compose stop master && docker-compose rm -f master"
+    printf "\n"
+    docker-compose stop master && docker-compose rm -f master
+}
+
+down_workers ()
+{
+    printf "\n\n===> TORN DOWN WORKER NODES"
+    printf "\n%s\n" "$HEADER"
+    echo "$ docker-compose stop worker && docker-compose rm -f worker"
+    printf "\n"
+    docker-compose stop master && docker-compose rm -f master
+}
+
+list ()
+{
+    printf "\n\n===> LIST CONTAINERS"
+    printf "\n%s\n" "$HEADER"
+    echo "$ docker-compose ps"
+    printf "\n"
+    docker-compose ps
+}
+
+
+exec_on_mpi_master_container ()
+{
+    # shellcheck disable=SC2046    
+    docker exec -it -u mpi $(docker-compose ps | grep 'master'| awk '{print $1}') "$@"
+}
+
+prompt_ready ()
+{
+    printf "\n\n===> CLUSTER READY \n\n"
+}
+
+show_instruction ()
+{
+    echo '                            ##         .          '
+    echo '                      ## ## ##        ==          '
+    echo '                   ## ## ## ## ##    ===          '
+    echo '               /"""""""""""""""""\___/ ===        '
+    echo '          ~~~ {~~ ~~~~ ~~~ ~~~~ ~~~ ~ /  ===- ~~~ '
+    echo '               \______ o           __/            '
+    echo '                 \    \         __/               '
+    echo '                  \____\_______/                  '
+    echo '                                                  '
+    echo '               Alpine MPICH Cluster v1.0          '
+    echo ''
+    echo ' More info: https://github.com/NLKNguyen/alpine-mpich-cluster'
+    echo ''
+    echo '=============================================================='
+    echo ''
+
+    echo "To run MPI programs in an interative shell:"
+    echo "  1. Login to master node:"
+    echo "     Using Docker through command wrapper:"
+    echo "     $ ./cluster.sh login"
+    echo ""
+    echo "     Or using SSH with keys through exposed port:"
+    echo "     $ ssh -o \"StrictHostKeyChecking no\" -i ssh/id_rsa -p $SSH_PORT mpi@localhost"
+    echo '       where [localhost] could be changed to the host IP of master node'
+    echo ""
+    echo "  2. Execute MPI programs inside master node, for example:"
+    echo "     $ mpirun hostname"
+    echo "      *----------------------------------------------------*"
+    echo "      | Default hostfile of connected nodes in the cluster |"
+    echo "      | is automatically updated at /etc/opt/hosts         |"
+    echo "      | To obtain hostfile manually: $ get_hosts > hosts   |"
+    echo "      * ---------------------------------------------------*"
+    echo ""
+    echo ""
+    echo "To run directly a shell command at master node:"
+    echo "     $ ./cluster.sh exec [COMMAND]"
+    echo ""
+    echo "     Example: "
+    echo "     $ ./cluster.sh exec mpirun hostname"    
+    echo ""
+}
+
+
+
+#############################################
+
+while [ "$1" != "" ];
+do
+    PARAM=$(echo "$1" | awk -F= '{print $1}')
+    VALUE=$(echo "$1" | awk -F= '{print $2}')
+
+    case $PARAM in
+        help)
+            usage
+            exit
+            ;;
+        -i)
+            show_instruction
+            exit
+            ;;
+
+        login)
+            COMMAND_LOGIN=1
+            ;;
+
+        exec)
+            COMMAND_EXEC=1
+            shift # the rest is the shell command to run in the node
+            SHELL_COMMAND="$*"
+            break # end while loop
+            ;;
+
+        up)
+            COMMAND_UP=1
+            ;;
+
+        down)
+            COMMAND_DOWN=1
+            ;;
+
+        reload)
+            COMMAND_RELOAD=1
+            ;;
+
+        scale)
+            COMMAND_SCALE=1
+            ;;
+
+        list)
+            COMMAND_LIST=1
+            ;;
+
+        size)
+            [ "$VALUE" ] && SIZE=$VALUE
+            ;;
+
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
+if [ $COMMAND_UP -eq 1 ]; then
+    down_all
+    up_registry
+    # generate_ssh_keys
+    build_and_push_image
+    up_master
+    up_workers
+
+    prompt_ready
+    show_instruction
+
+elif [ $COMMAND_DOWN -eq 1 ]; then
+    down_all
+
+elif [ $COMMAND_SCALE -eq 1 ]; then
+    down_master
+    down_workers
+    up_master
+    up_workers
+
+    prompt_ready
+    show_instruction
+
+elif [ $COMMAND_RELOAD -eq 1 ]; then
+    down_master
+    down_workers
+    build_and_push_image
+    up_master
+    up_workers
+
+    prompt_ready
+    show_instruction
+
+elif [ $COMMAND_LOGIN -eq 1 ]; then
+    exec_on_mpi_master_container ash
+
+elif [ $COMMAND_EXEC -eq 1 ]; then
+    exec_on_mpi_master_container ash -c "${SHELL_COMMAND}"
+
+elif [ $COMMAND_LIST -eq 1 ]; then
+    list
+else
+    usage
+fi
